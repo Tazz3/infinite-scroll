8751670cf493d16286cf337b358c67773a890fab
mariam gvaramia <gvaramia.mariam@yahoo.com>
2023-03-16 21:16:59 +0400

    applied the existing changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [thisState, setThisState] = useState([]);
+  const [curPage, setCurPage] = useState(true);
+  const [perPage, setPerPage] = useState(20);
+  const [pageNum, setPageNum] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const characters = async () => {
+      const response = await fetch(
+        `http://sweeftdigital-intern.eu-central-1.elasticbeanstalk.com/user/${pageNum}/${perPage}`
+      );
+      const newData = await response.json();
+
+      setThisState((old) => [...old, ...newData.list]);
+
+      console.log(thisState, "this is the state");
+      setIsLoading(false);
+    };
+    characters();
+  }, [pageNum]);
+
+  useEffect(() => {
+    function handleScroll() {
+      const windowHeight = window.innerHeight;
+      const scrollHeight = document.documentElement.scrollHeight;
+      const scrollTop =
+        window.pageYOffset || document.documentElement.scrollTop;
+
+      if (scrollTop + windowHeight >= scrollHeight) {
+        setPageNum((prev) => (prev += 1));
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        {curPage &&
+          thisState.map((item) => (
+            <div
+              key={item.id}
+              style={{
+                height: "auto",
+                width: "25%",
+                border: "2px solid black",
+              }}
+            >
+              <h1>{item.name}</h1>
+              <h1>{item.lastName}</h1>
+              <h1>{item.prefix}</h1>
+              <h1>{item.title}</h1>
+              <img
+                src={item.imageUrl + `?v=${item.id}`}
+                style={{ height: "200px", width: "200px" }}
+                alt="charImage"
+              />
+            </div>
+          ))}
+
+        {isLoading && (
+          <div className="lds-facebook loading">
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
